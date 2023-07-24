3b13ac0eebcbd1791a8a3fd057b09c17f2e1885d
Nicro01 <110069146+Nicro01@users.noreply.github.com>
2023-07-23 21:48:25 -0300

    Update NoAD.py

--- NoAD.py
+++ NoAD.py
@@ -1,106 +1,70 @@
-from cgitb import text
-from select import select
 import shutil
-from tkinter import *
-from tkinter import filedialog
-import tkinter
-from turtle import bgcolor
-from PIL import Image,ImageTk
-from moviepy import *
-import moviepy.editor as mp
+import tkinter as tk
+from PIL import Image, ImageTk
+from tkinter import ttk
 from moviepy.editor import VideoFileClip
 from pytube import YouTube
+from tkinter import filedialog
 import urllib.request, io
-import re, os,io
+import moviepy as mp
+import re
+import os
+import threading
 
-screen = Tk()
-title = screen.title("NoAD")
-canvas = Canvas(screen, width= 600, height= 600)
-canvas.pack()
-screen.resizable(False,False)
-
-#Functions
 
 def select_path():
-    #Allows user to select a path from the explorer
     path = filedialog.askdirectory()
     path_label.config(text=path)
 
-def download_video():
-    #Get user path
-    get_link = link_field.get()
+
+def download_progress(stream, chunk, bytes_remaining):
+    total_size = stream.filesize
+    bytes_downloaded = total_size - bytes_remaining
+    download_percentage = (bytes_downloaded / total_size) * 100
+    progress_bar['value'] = download_percentage
+    root.update_idletasks()
 
 
-    #Get selected path
+def download_video(video_link):
     user_path = path_label.cget("text")
-    screen.title("Downloading...")
+    root.title("Downloading...")
 
-    #Get thumbnail
-    yt = YouTube(get_link)
+    yt = YouTube(video_link, on_progress_callback=download_progress)
     raw_data = urllib.request.urlopen(yt.thumbnail_url).read()
     im = Image.open(io.BytesIO(raw_data)).resize((200, 200))
     image = ImageTk.PhotoImage(im)
-    label = Label(image = image)
+    label = tk.Label(image=image)
     label.image = image
-    canvas.create_window(450,450,window=label)
+    canvas.create_window(450, 450, window=label)
 
-    #Get author
-    author = Label(text=yt.author)
-    canvas.create_window(80,380, window=author)
+    author = tk.Label(text=yt.author)
+    canvas.create_window(80, 380, window=author)
 
-    #Get video title
-    titl = Label(text=yt.title)
-    canvas.create_window(120,360, window=titl)
+    titl = tk.Label(text=yt.title)
+    canvas.create_window(120, 360, window=titl)
 
-    #Get video lenght
-    vid_lenght = Label(text=yt.length)
-    canvas.create_window(80,400, window=vid_lenght)
+    vid_lenght = tk.Label(text=yt.length)
+    canvas.create_window(80, 400, window=vid_lenght)
 
-
-    #Download Video
-    mp4_video = YouTube(get_link).streams.get_highest_resolution().download()
-    vid_clip = VideoFileClip(mp4_video)
-    vid_clip.close()
-    #Move file to selected directory
-    shutil.move(mp4_video, user_path)
-    screen.title("Download Complete! Downlaod another file...")
-
-def download_audio():
-    #Get user path
-    get_link = link_field.get()
+    stream = yt.streams.get_highest_resolution()
+    t = threading.Thread(target=download_video_thread, args=(stream, user_path))
+    t.start()
 
 
-    #Get selected path
+def download_video_thread(stream, user_path):
+    stream.download(user_path)
+    root.title("Download Complete! Download another file...")
+    progress_bar['value'] = 0
+
+
+def download_audio(video_link):
     user_path = path_label.cget("text")
-    screen.title("Downloading...")
+    root.title("Downloading...")
 
-    #Get thumbnail
-    yt = YouTube(get_link)
-    raw_data = urllib.request.urlopen(yt.thumbnail_url).read()
-    im = Image.open(io.BytesIO(raw_data)).resize((200, 200))
-    image = ImageTk.PhotoImage(im)
-    label = Label(image = image)
-    label.image = image
-    canvas.create_window(450,450,window=label)
-
-    #Get author
-    author = Label(text=yt.author)
-    canvas.create_window(80,380, window=author)
-
-    #Get video title
-    titl = Label(text=yt.title)
-    canvas.create_window(120,360, window=titl)
-
-    #Get video lenght
-    vid_lenght = Label(text=yt.length)
-    canvas.create_window(80,400, window=vid_lenght)
-
-
-    #Download Audio
-
+    yt = YouTube(video_link, on_progress_callback=download_progress)
     ys = yt.streams.filter(only_audio=True).first().download(user_path)
 
-    screen.title('Convertento arquivo')
+    root.title('Converting file')
     for file in os.listdir(user_path):
         if re.search('mp4', file):
             mp4_path = os.path.join(user_path, file)
@@ -109,66 +73,38 @@ def download_audio():
             new_file.write_audiofile(mp3_path)
             os.remove(mp4_path)
 
+    root.title("Download Complete! Download another file...")
+    progress_bar['value'] = 0
 
 
-    #Move file to selected directory
-    screen.title("Download Complete! Downlaod another file...")
+root = tk.Tk()
+title = root.title("NoAD Downloader")
+canvas = tk.Canvas(root, width=600, height=600)
+canvas.pack()
+root.resizable(False, False)
 
-
-    
-
-
-
-
-# Logo - Imagem
-logo_text = Label(text="NoAD Downloader" ,font=('Arial', 20), bg=None, anchor=N)
-
+logo_text = tk.Label(text="NoAD Downloader", font=('Arial', 20), bg=None)
 logo_text.pack()
 
-logo_img = (Image.open("logo-NoAD.png"))
 
-#Resize
+link_field = tk.Entry(root, width=50)
+link_label = tk.Label(root, text="Video link: ", font=('Arial', 10)).place(x=76, y=190, width=70, height=20)
 
-resized_logo_img = logo_img.resize((615,600), Image.ANTIALIAS)
-new_image= ImageTk.PhotoImage(resized_logo_img)
-
-canvas.create_image(300, 300,image=new_image)
-
-#Link Field
-
-link_field = Entry(screen, width=50)
-link_label = Label(screen, text = "Video link: ", font=('Arial', 10)).place(x=76,y=190,width=70, height=20)
-
-#Select Path for saving the file
-
-path_label = Label(screen, text= "Select path for download", font=("Arial", 12))
-select_btn = Button(screen, text="Select", command=select_path)
-
-#Add to window
+path_label = tk.Label(root, text="Select path for download", font=("Arial", 12))
 path_label.config(bg="#FFF")
 canvas.create_window(300, 250, window=path_label)
+select_btn = tk.Button(root, text="Select", command=select_path)
 canvas.create_window(300, 280, window=select_btn)
-
-
-#Add widgets to window
 
 canvas.create_window(115, 200, window=link_label)
 canvas.create_window(300, 200, window=link_field)
 
-#Download Buttons
-
-download_btn = Button(screen, text="Download Video", command=download_video)
-download_audio_btn = Button(screen, text="Download Audio", command=download_audio)
-
-
-#Add to canvas
-
+download_btn = tk.Button(root, text="Download Video", command=lambda: download_video(link_field.get()))
+download_audio_btn = tk.Button(root, text="Download Audio", command=lambda: download_audio(link_field.get()))
 canvas.create_window(530, 200, window=download_btn)
 canvas.create_window(530, 170, window=download_audio_btn)
 
+progress_bar = ttk.Progressbar(root, orient=tk.HORIZONTAL, length=400, mode='determinate')
+canvas.create_window(300, 500, window=progress_bar)
 
-
-
-
-
-screen.mainloop()
+root.mainloop()
